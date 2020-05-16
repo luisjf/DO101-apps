146d6deaee3e0276d8ae213258a26aa75921a2ff
José Francisco <jfluis@atsistemas.com>
2020-05-16 14:17:44 +0200

    actualiza versión de la aplicación

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 1 of the app.' + '\n';
+    response = 'updated app to v2 .' + '\n';
 
     //send the response to the client
     res.send(response);
